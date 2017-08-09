bf94a4692b8ed1c5b84c32f727aeae2403746883
stephinwv <thekingdomseeker@aol.com>
2017-08-09 11:57:26 -0400

    adding files to get heroku

---
+++ views/index.rb
@@ -0,0 +1,17 @@
+<!doctype HTMLS>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>Our first CSS </title>
+	<meta name ="description" content="Our First CSS">
+	<meta name ="author" content="sitepoint">
+
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+</body>
+</html>
